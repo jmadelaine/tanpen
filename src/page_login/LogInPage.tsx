ce2011228fbb72eb29c0e8c01bae1ccc30943ff6
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2026-08-12 22:09:07 +0100

    WIP error handling to come, form structure sorted

--- src/page_login/LogInPage.tsx
+++ src/page_login/LogInPage.tsx
@@ -2,14 +2,18 @@ import { useRouter } from '@tanstack/react-router';
 import { useMutation } from '@tanstack/react-query';
 import { logInWithPassword } from '../auth/api';
 import { setTokens } from '../auth/session';
+import { useState } from 'react';
 
 export function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const router = useRouter();
   const logInMutation = useMutation({
     mutationFn: () =>
       logInWithPassword({
-        email: 'jonny@example.com',
-        password: 'password123',
+        email: email,
+        password: password,
       }),
     onSuccess: (response) => {
       if ('accessToken' in response) {
@@ -20,17 +24,41 @@ export function LoginPage() {
         void router.invalidate();
       }
     },
+    onError: () => {},
   });
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    logInMutation.mutate();
+  };
 
   return (
     <div>
-      <button
-        type="button"
-        onClick={() => logInMutation.mutate()}
-        disabled={logInMutation.isPending}
-      >
-        Log in as Jonny
-      </button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">{'Email'}</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+
+        <label htmlFor="password">{'Password'}</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+
+        <button type="submit" disabled={logInMutation.isPending}>
+          Log in
+        </button>
+      </form>
     </div>
   );
 }
