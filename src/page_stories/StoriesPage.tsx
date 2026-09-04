c676f1f7348c3a5f820bf265aeaadc24ccdaba6d
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2026-09-04 16:03:08 +0100

    start of stories page

--- src/page_stories/StoriesPage.tsx
+++ src/page_stories/StoriesPage.tsx
@@ -1,1 +1,17 @@
-export const storiesPage = () => {};
+import { useQuery } from '@tanstack/react-query';
+import { storiesQueryOptions } from '../page_home/storiesQueryOptions';
+
+export const StoriesPage = () => {
+  const storiesQuery = useQuery(storiesQueryOptions());
+  console.log(storiesQuery.data);
+
+  if (storiesQuery.isError) {
+    return <div>{'Error loading stories.'}</div>;
+  }
+
+  if (storiesQuery.isLoading) {
+    return <div>{'Loading stories...'}</div>;
+  }
+
+  return <div className="flex items-center justify-center max-w-3xl px-4 py-10 gap-3"></div>;
+};
--- src/routes/_authenticated/stories.tsx
+++ src/routes/_authenticated/stories.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { storiesPage } from '../../page_stories/StoriesPage';
+import { StoriesPage } from '../../page_stories/StoriesPage';
 
 export const Route = createFileRoute('/_authenticated/stories')({
-  component: storiesPage,
+  component: StoriesPage,
 });
